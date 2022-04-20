c20257b15889fb0e41c89266188ac0e5d51bca1c
Nick Lee <nickaustinlee@gmail.com>
2022-04-20 14:18:29 -0700

    Bumping to v 0.1.2
    Problem with release required an increment on version number to avoid conflicts.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="labelsnow",
-    version="0.1.1",
+    version="0.1.2",
     author="Labelbox",
     author_email="ecosystem+snowflake@labelbox.com",
     description="Labelbox Connector for Snowflake",
