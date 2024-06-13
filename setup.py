5ebad5463f3defb555061705666a87a6c29b7f34
Ramy Fekry ramy1951@gmail.com <ramy1951@live.com>
2024-06-13 13:00:42 -0700

    setup.py 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="labelsnow",
-    version="0.1.4",
+    version="1.0.0",
     author="Labelbox",
     author_email="ecosystem+snowflake@labelbox.com",
     description="Labelbox Connector for Snowflake",
